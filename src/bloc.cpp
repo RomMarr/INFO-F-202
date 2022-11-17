58caf0d5d7109191481adce4677349fd2c91aa51
RomMarr <romain.markowitch@gmail.com>
2022-11-17 17:14:18 +0100

    pas héritage

--- src/bloc.cpp
+++ src/bloc.cpp
@@ -1,77 +1,14 @@
 #include "bloc.h"
+#include <FL/Fl.H>
 
-Bloc::Bloc() 
 
-virtual void Bloc::draw() const=0;
+Bloc::Bloc(int weight, Fl_Color color):weight{weight}, color{color}{};
 
-virtual void Bloc::setWeight(int new_weight) const=0;
+void Bloc::draw() {
+};
 
-virtual void Bloc::setColor(string color) const=0;
+void Bloc::setWeight(int new_weight){
+    weight = new_weight; };
 
-Target::Target():weight{-1}, color{"red"}
-
-void Target::setWeight(int new_weight){
-    weight = new_weight; }
-
-void Target::setColor(string new_color){
-    color = new_color; }
-
-void Target::draw(){
-
-}
-
-Wall::Wall():weight{10}, color{"black"}
-
-void Wall::setWeight(int new_weight){
-    weight = new_weight; }
-
-void Wall::setColor(string new_color){
-    color = new_color; }
-
-void Wall::draw(){
-    
-}
-
-Floor::Floor():weight{-1}, color{"gray"}
-
-void Floor::setWeight(int new_weight){
-    weight = new_weight; }
-
-void Floor::setColor(string new_color){
-    color = new_color; }
-
-void Floor::draw(){
-    
-}
-
-Box::Box():weight{6}, color{"brown"}
-
-void Box::setWeight(int new_weight){
-    weight = new_weight; }
-
-void Box::setColor(string new_color){
-    color = new_color; }
-
-void Box::draw(){
-    
-}
-
-void Box::move(){
-
-}
-
-Teleporter::Teleporter():weight{-1}, color{"purple"}
-
-void Teleporter::setWeight(int new_weight){
-    weight = new_weight; }
-
-void Teleporter::setColor(string new_color){
-    color = new_color; }
-
-void Teleporter::draw(){
-    
-}
-
-void Teleporter::teleport(){
-
-}
+void Bloc::setColor(Fl_Color new_color){
+    color = new_color; };
--- src/bloc.h
+++ src/bloc.h
@@ -3,75 +3,20 @@
 
 #include "stdio.h"
 #include <string>
+#include <FL/Fl.H>
 
 using namespace std;
 
 
 class Bloc {
     int weight;
-    string color;
+    Fl_Color color;
 public:
-    Bloc() ;
-    virtual void draw();
-    virtual void setWeight(int weight);
-    virtual void setColor(string color);
-    //~Bloc()
-};
-
-class Target:public Bloc{
-    int weight;
-    string color;
-public:
-    Target();
+    Bloc(int weight, Fl_Color color) ;
     void draw();
-    void setWeight(int weight) ;
-    void setColor(string color) ;//override ?
-    //~Target()
-};
-
-class Wall:public Bloc{
-    int weight;
-    string color;
-public:
-    Wall();
-    void draw();
-    void setWeight(int weight) ;
-    void setColor(string color); //override ?
-    //~Wall()
-};
-
-class Floor:public Bloc{
-    int weight;
-    string color;
-public:
-    Floor();
-    void draw();
-    void setWeight(int weight) ;
-    void setColor(string color); //override ?
-    //~Floor()
-};
-
-class Box:public Bloc{
-    int weight;
-    string color;
-public:
-    Box();
-    void draw();
-    void setWeight(int weight) ;
-    void setColor(string color); //override ?
-    void move();
-    //~Box()
-};
-
-class Teleporter:public Bloc{
-    int weight;
-    string color;
-public:
-    Teleporter();
-    void draw();
-    void setWeight(int weight) ;
-    void setColor(string color); //override ?
-    void teleport();
+    void setWeight(int weight);
+    void setColor(Fl_Color color);
+    ~Bloc();
 };
 
 #endif
